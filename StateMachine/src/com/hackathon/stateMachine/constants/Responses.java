886db8d16e13d9c4919645e31da6f73ea9e7102f
Steven Hynes <shynes3@gatech.edu>
2019-10-26 22:49:37 -0400

    added latter six response arrays

--- StateMachine/src/com/hackathon/stateMachine/constants/Responses.java
+++ StateMachine/src/com/hackathon/stateMachine/constants/Responses.java
@@ -17,6 +17,23 @@ public class Responses {
     public static final String[] fearResponseArray = new String[]{"That sounds scary", "Yikes",
             "I can see why you would be apprehensive", "Sounds like a scary situation to me"};
 
+    public static final String[] happyResponseArray = new String[]{"Sounds like you're having a good day!",
+            "I'm glad you're happy!"}
+
+    public static final String[] sadResponseArray = new String[]{"Sorry that you're feeling under the weather today.",
+            "You seem sad; sorry you're not having a great day."}
+
+    public static final String[] likeResponseArray = new String[]{"Glad you're pleased!", "Sounds like you like that!"}
+
+    public static final String[] dislikeResponseArray = new String[]{"Sorry you're displeased.",
+            "Sounds like you're not super pleased about that."}
+
+    public static final String[] sureResponseArray = new String[]{"Sounds like you're confident about that!",
+            "Sounds like you're very sure!", "I like your confidence!", "Sounds like you know what you're doing!"}
+
+    public static final String[] unsureResponseArray = new String[]{"Are you sure about that?",
+            "You don't sound so sure.", "Sounds like you're unsure about that."}
+
     public static String getGreeting() {
         return greeting;
     }
