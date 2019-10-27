b1511780bef73cd3b7d6b328bba13c471c861f82
Steven Hynes <shynes3@gatech.edu>
2019-10-27 03:54:05 -0400

    modified emotions for new API

--- PechaKucha/app/src/stateMachine/constants/Responses.java
+++ PechaKucha/app/src/stateMachine/constants/Responses.java
@@ -20,33 +20,17 @@ public class Responses {
     private static final String[] fearResponseArray = new String[]{"That sounds scary", "Yikes",
             "I can see why you would be apprehensive", "Sounds like a scary situation to me"};
 
-    private static final String[] shameResponseArray = new String[]{"That sounds a little embarrassing",
-            "Embarrassment can be a powerful emotion", "Sound's like you're having a very emotional experience"};
+    private static final String[] excitedResponseArray = new String[]{"Wow! That's very surprising",
+            "Sounds like you're excited", "Wow!"};
 
-    private static final String[] surpriseResponseArray = new String[]{"Wow! That's very surprising",
-            "Sounds like you're a little surprised", "Wow!"};
-
-    private static final String[] calmnessResponseArray = new String[]{"Sounds like you've got everything under control",
-            "Interesting....", "You seem pretty calm about this."};
-
-    private static final String[] agitationResponseArray = new String[]{"Sounds like a stressful situation, but I trust you'll make the right decision",
-            "Hmm, I would be stressed too."};
     private static final String[] happyResponseArray = new String[]{"Sounds like you're having a good day!",
             "I'm glad you're happy!"};
 
     private static final String[] sadResponseArray = new String[]{"Sorry that you're feeling under the weather today.",
             "You seem sad; sorry you're not having a great day."};
 
-    private static final String[] likeResponseArray = new String[]{"Glad you're pleased!", "Sounds like you like that!"};
-
-    private static final String[] dislikeResponseArray = new String[]{"Sorry you're displeased.",
-            "Sounds like you're not super pleased about that."};
-
-    private static final String[] sureResponseArray = new String[]{"Sounds like you're confident about that!",
-            "Sounds like you're very sure!", "I like your confidence!", "Sounds like you know what you're doing!"};
-
-    private static final String[] unsureResponseArray = new String[]{"Are you sure about that?",
-            "You don't sound so sure.", "Sounds like you're unsure about that."};
+    private static final String[] boredResponseArray = new String[]{"Sorry you're bored.",
+            "Sounds like you're not super excited about that."};
 
     public static String getGreeting() {
         return greeting;
@@ -72,36 +56,17 @@ public class Responses {
         return fearResponseArray[random.nextInt(fearResponseArray.length)];
     }
 
-    public static String getShameResponse(){
-        return shameResponseArray[random.nextInt(shameResponseArray.length)];
-    }
-    public static String getSurpriseResponse() {
-        return surpriseResponseArray[random.nextInt(surpriseResponseArray.length)];
+    public static String getExcitedResponse() {
+        return excitedResponseArray[random.nextInt(excitedResponseArray.length)];
     }
 
-    public static String getCalmnessResponse() {
-        return calmnessResponseArray[random.nextInt(calmnessResponseArray.length)];
-    }
-
-    public static String getAgitationResponse() {
-        return agitationResponseArray[random.nextInt(agitationResponseArray.length)];
-    }
-
-    public static String getUnsureResponse() {
-        return unsureResponseArray[random.nextInt(unsureResponseArray.length)];
-    }
-    public static String getSureResponse() {
-        return sureResponseArray[random.nextInt(sureResponseArray.length)];
-    }
-    public static String getDislikeResponse() {
-        return dislikeResponseArray[random.nextInt(dislikeResponseArray.length)];
-    }
-    public static String getLikeResponse() {
-        return likeResponseArray[random.nextInt(likeResponseArray.length)];
+    public static String getBoredResponse() {
+        return boredResponseArray[random.nextInt(boredResponseArray.length)];
     }
     public static String getSadResponse() {
         return sadResponseArray[random.nextInt(sadResponseArray.length)];
     }
+
     public static String getHappyResponse() {
         return happyResponseArray[random.nextInt(happyResponseArray.length)];
     }
